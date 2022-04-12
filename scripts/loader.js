7035a77cdcfc015978b6a14b35dc9aa175075a00
byrozaurowe <wiktoriabyra99@gmail.com>
2022-04-12 10:20:55 +0200

    added website

---
+++ scripts/loader.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    document.documentElement.scrollTop = 0;
+    document.body.style.overflowY = "auto";
+    document.querySelector(".loader-overlay").classList.add("hidden");
+    setupAnimations();
+  };
+  
